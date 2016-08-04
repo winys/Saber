72459078abe0ce017d4ae306d39cc1248f765174
uk9921 <798226672@qq.com>
2016-08-04 12:12:06 +0800

    second commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// 获取依赖
+var gulp = require('gulp'), 
+  childProcess = require('child_process'), 
+  electron = require('electron-prebuilt');
+// 创建 gulp 任务
+gulp.task('run', function () { 
+  childProcess.spawn(electron, ['.'], { stdio: 'inherit' }); 
+});
