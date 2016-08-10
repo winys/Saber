eafc53c3403bb033d1ac0dddbca36a39a4c531ce
winys <824350760@qq.com>
2016-08-10 16:44:42 +0800

    context init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# Saber" 
+
+Make your own toolkit
---
+++ app/src/components/Hello.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1 @click="cll">{{ msg }}</h1>
+    <h3>{{tt}}</h3>
+    </div>
+  </body>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initiadl state.
+      msg: 'Hello World!',
+      tt : "winys"
+    }
+  },
+  methods:{
+    cll (){
+      this.msg = "Hello World!";
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
